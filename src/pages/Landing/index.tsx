12c4c7effd44f4611ec902bab390e030e687c6d0
Samuel <samuellfa3@gmail.com>
2020-12-09 22:54:20 -0300

    Add tech cards and presentation

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -22,8 +22,55 @@ function Landing() {
                     <h3>Hi, I’m Samuel Nice to meet you.</h3>
                     <div className="presentation-text">
                         <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat massa orci, nec faucibus tortor vehicula non. Praesent eu suscipit nisi. Fusce in bibendum neque. Ut sapien urna, dapibus eu hendrerit faucibus, fringilla et mi. Etiam nunc nisi, pharetra id sapien quis, aliquam consequat lectus. Curabitur eget massa lacus. Morbi quis elit non justo dictum consectetur. Curabitur vitae leo quis metus faucibus maximus at at massa. Proin sodales ullamcorper nisl sed egestas. Vestibulum ultricies libero turpis, quis finibus tortor aliquet sit amet. Nam aliquet nec libero a volutpat. Vivamus nec scelerisque nunc. Suspendisse tempus rutrum justo, in volutpat sem. Mauris eget suscipit libero.
+                            A computer scientist who learned his first line of code in the year 2015 and is now passionate about the back-end world.
+                            I am a qualified and curious developer with the desire to learn something new every day.
+                            A person with great adaptability and able to deal with the problems that are presented.
                         </p>
+                    </div>
+                </div>
+
+                <div className="tech">
+                    <div className="card">
+                        <h3>Back-end</h3>
+                        <div className="groups">
+                            <div className="tech-group">
+                                <h4>Languages</h4>
+                                <ul>
+                                    <li>Javascript</li>
+                                    <li>Elixir</li>
+                                    <li>Java</li>
+                                    <li>Python</li>
+                                </ul>
+                            </div>
+
+                            <div className="tech-group">
+                                <h4>Frameworks</h4>
+                                <ul>
+                                    <li>Node.js</li>
+                                    <li>Phoenix</li>
+                                    <li>Spring</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <h3>Database</h3>
+                        <div className="groups">
+                            <div className="tech-group">
+                                <h4>SQL</h4>
+                                <ul>
+                                    <li>PostgreSQL</li>
+                                    <li>SQL Server</li>
+                                </ul>
+                            </div>
+
+                            <div className="tech-group">
+                                <h4>NoSQL</h4>
+                                <ul>
+                                    <li>MongoDB</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
